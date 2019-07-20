605f19c8459ebfe4644c4848ebd3d80610d8bffd
Panhungting <a0910827893@gmail.com>
2019-07-20 13:10:49 +0900

    002

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0720_Git02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(1234567880);
+        }
+    }
+}
